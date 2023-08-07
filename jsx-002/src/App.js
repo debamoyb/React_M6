be76165ad2e869a3885399ed181826368737a8d1
debamoyb <debamoyb@gmail.com>
2023-08-07 19:39:16 +0530

    Files Uploaded

---
+++ jsx-002/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import { SiAirbnb } from 'react-icons/si';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className='main'>
+        <h1>The Evolution of <br/> Airbnb's Frontend</h1>
+        <h3>@spikebrehm</h3>
+        <h4><SiAirbnb/></h4>
+        <h4 className='foot'>Airbnb</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
