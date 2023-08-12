fa8df3369eca2d4ad472867d72b71fd8d9921a87
debamoyb <debamoyb@gmail.com>
2023-08-13 01:59:27 +0530

    Files Updated

--- react-props-002/src/Components/Child.jsx
+++ react-props-002/src/Components/Child.jsx
@@ -1,61 +1,42 @@
 import React from 'react';
-import { RiInstallLine } from 'react-icons/ri';
+import { MdOutlineBookmarkAdd } from 'react-icons/md';
 
 function Child(props) {
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-around",
-          margin: "16px",
-          boxShadow: "1px 1px 10px black",
-        }}
-      >
+      <div style={{display:"flex",justifyContent:"space-around",margin:"10px",fontFamily:"'vrinda bold', sans-serif"}}>
         <div style={{ width: "60%" }}>
           <div style={{ display: "flex", gap: "8px" }}>
             <span style={{ display: "flex", alignItems: "center", }}>
               <img style={{borderRadius:"50%"}} src={props.thumbImg} width="20px" height="20px" alt={props.altthumb}/>
             </span>
-            <h3>{props.name}</h3>
+            <h3 style={{color:"dimGray"}}>{props.name}</h3>
           </div>
-          <h2 style={{ marginTop: "-10px" }}>{props.heading}</h2>
-          <p style={{ marginTop: "-10px" }}>{props.para}</p>
+          <div style={{ textAlign:"left"}}>
+            <h1 style={{ marginTop: "-10px", fontWeight:"900" }}>{props.heading}</h1>
+            <p style={{ marginTop: "-10px", fontWeight:"500",color:"Gray",fontSize:"26.7px" }}>{props.para}</p>
+          </div>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div style={{ display: "flex", gap: "10px", marginTop: "-16px" }}>
-              <p>{props.date}</p>
-              <p>{props.time}</p>
-              <p
-                style={{
-                  border: "1px solid white",
-                  backgroundColor: "Lightgray",
-                  borderRadius: "10px",
-                  height: "30px",
-                  width: "60px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                {props.cat}
-              </p>
+            <div style={{ display: "flex", gap: "10px", marginTop: "-16px", fontWeight:"600",color:"Gray",fontSize:"18px"}}>
+              <p>{props.date}&nbsp;&nbsp;&#183;</p>
+              <p>{props.time}&nbsp;&nbsp;&#183;</p>
+              <button style={{border:"none",background:"gainsboro",borderRadius:"20px",color:"grey",fontSize:"18px",marginTop:"10px",padding:"15px",fontWeight:"600",height:"35px",width:"auto",display:"flex",justifyContent:"center",alignItems:"center",}}>{props.cat}</button>
             </div>
-            <div>
-              <p>
-                <RiInstallLine />
-              </p>
+            <div style={{color:"gray"}}>
+              <h1>
+                <MdOutlineBookmarkAdd />
+              </h1>
             </div>
           </div>
         </div>
         <div
           style={{
             width: "30%",
-            marginTop: "20px",
             display: "flex",
-            alignItems: "center",
+            alignItems: "center"
           }}
         >
-          <img src={props.mainImg} height="180px" width="350px" alt={props.altImg}/>
+          <img style={{objectFit:"cover"}} src={props.mainImg} height="250px" width="380px" alt={props.altImg}/>
         </div>
       </div>
     </div>
